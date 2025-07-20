d6c023f26e6cb5a79d80abaf772426816cc4b9ed
Yea Ahmed Nipul <136602852+YeaNipul@users.noreply.github.com>
2025-07-20 16:15:32 +0600

    Registration page

--- Mid/lab_1.php
+++ Mid/lab_1.php
@@ -1,1 +1,85 @@
+<!DOCTYPE html>
+<html>
 
+<body>
+    <center>
+    <p style="color:blue" <h1> AIUB </h>
+    <p style="color:blue" <h2>Course Registation from</h2>
+
+    </center>
+    <h3 align ="left"> Compute the Registation</h3>
+    <table>
+        <tr>
+            <td> Full name:</td><td><input type="text"><br></td>
+
+        </tr>
+
+        <tr>
+            <td>Emaill:</td><td><input type="text"><br></td>
+        </tr>
+
+        <tr>
+            <td>Passwoard:</td><td><input type="text"><br></td>
+        </tr>
+
+        <tr>
+            <td>
+                Gandere:</td><td><input type="radio"name="des">Male 
+                <input type="radio"name="des">Femal
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                language known:</td><td><input type="radio"name="des">English
+                 <input type="radio"name="des">Bangla
+                <input type="radio"name="des">hindi
+            </td>    
+        </tr>
+
+        <tr>
+            <td>
+                Country name:</td><td><Select name="" id="">
+                <option value="">--select--</option>
+                <option value="">Bangladesh</option>
+                <option value="">Canada</option>
+                <option value="">Thailand</option>
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                Date of birth:</td><td><input type="date"> </td>
+            </td>
+        </tr>
+
+         <tr>
+            <td>
+                Upload photo:</td><td><input type="file"> </td>
+            </td>
+        </tr>
+
+         <tr>
+            <td>
+                Comment:</td><td><textarea type=""" id="" cols="35" rows="7"> </textarea></td>
+            </td>
+        </tr>
+
+         <tr>
+            <td></td>
+            <td>
+                <button>Registetr</button>
+            </td>
+        </tr>
+
+
+
+    </table>
+     
+
+   
+
+</body>
+
+
+</html>
