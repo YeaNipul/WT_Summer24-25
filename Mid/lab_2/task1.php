9189772ec3f13161baa02b3a85a53ce300747993
YeaNipul <yeanipul34@gmail.com>
2025-07-26 16:50:34 +0600

    Adding lab2

---
+++ Mid/lab_2/task1.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+      .flow{
+        height: 200px;
+        width: 400px;
+        overflow-y:  scroll;
+
+      }
+      body{
+        backgrond-color:skyblue;
+      }
+    </style>
+</head>
+<body>
+    <center>
+    <h1 style="color:blue" > Bank Management System </h1>
+    <h2 style="color:blue" > Your tursted Financial Patner </h2>
+    </center>
+    <h3 align="left" > Coustomer registration from </h3>
+    <div>
+    <table>
+        <tr>
+            <td>Full name:</td><td><input type="text"><br></td>        
+        </tr>
+       <tr>
+        <td>
+            Date of birth:</td><td><input type="date">
+        </td>
+        </tr>
+
+        <tr>
+            <td>
+            Gander:</td><td><input type="radio"name="des">Male
+            <input type="radio"name="des">Femal
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                Marital Status:</td>
+                <td>
+                    <select>
+                        <option value="Single">single</option>
+                        <option value="Married">Married</option>
+                    </select>
+            </td>
+        </tr>    
+        
+        <tr>
+            <td>Account type:</td>
+            <td>
+                <select>
+                    <option value="Savings">Savings</option>
+                    <option value="Current">Current</option>
+                    <option value="fixed">fixed</option>
+                </select>
+            </td>
+        </tr>
+
+        <tr>
+            <td>Initial Deposit Amount:</td>
+            <td><input type="number"></td>
+        </tr>
+        
+        <tr>
+            <td>Email Adress:</td>
+            <td><input type="email"></td>
+        </tr>
+
+        <tr>
+            <td>Address:</td>
+            <td><input type="text"></textarea></td>
+        </tr>
+
+        <tr>
+            <td>Ocupation:</td>
+             <td><input type="text"></td>
+
+        </tr>
+
+         <tr>
+            <td>National id (NID):</td>
+             <td><input type="number"></td>
+
+        </tr>
+
+        <tr>
+            <td>Passwoard:</td><td><input type="text"><br></td>
+        </tr>
+
+         <tr>
+            <td>upload id proof:</td>
+             <td><input type="file"></td>
+
+        </tr>
+
+
+
+     </table>
+     <input taype="chekckbox" name="terms"> I agree the tearm and coundition
+     <br>
+     <center>
+        <input type="submit" value="Register">
+        <input type="reset" value="clear">
+    </center>
+
+</div>
+<div class = "flow">
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio placeat quia sequi ipsam nemo, voluptatibus magnam. Nisi eveniet consequatur veritatis odio error reiciendis perspiciatis modi maxime ad laudantium itaque, laborum quo non inventore dolores eius enim earum reprehenderit, iste officiis iusto expedita. Omnis quo officia asperiores fugiat dolorum aut necessitatibus eaque a impedit, consequuntur, deleniti ducimus consequatur repudiandae ipsum laudantium et numquam quod eligendi ea facere! Architecto cupiditate optio molestias beatae veniam deleniti, quas, deserunt sapiente illo saepe eaque distinctio esse? Minima officia eligendi veritatis. Voluptates optio velit debitis ut rem magni dolore, modi laborum rerum veniam architecto ratione? Ab, adipisci expedita magni iste praesentium ex est nobis ea corrupti totam natus architecto eius fugit omnis rerum pariatur, tenetur vero dicta quod! Odit accusantium iusto atque soluta laudantium voluptas eveniet doloribus eum nulla vitae. Eaque obcaecati, voluptatibus ipsa quidem natus fugiat nesciunt? Eaque aperiam tenetur nulla perspiciatis hic est numquam?
+</div>
+     
+
+
+
+
+
+
+</body>
+</html>
