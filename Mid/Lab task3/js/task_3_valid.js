32a5c59f4e7ce523821e9a06c569e56b4a1a90b4
YeaNipul <yeanipul34@gmail.com>
2025-08-03 16:52:49 +0600

    commit

--- Mid/Lab task3/js/task_3_valid.js
+++ Mid/Lab task3/js/task_3_valid.js
@@ -1,1 +1,32 @@
-var   
+    var fromname = document.getElementById("fromname").value;
+    var name = document.getElementById("name").value;
+    var Lastname = document.getElementById("Last name").value;
+    var Address = document.getElementById("Address").value;
+    var City = document.getElementById("City").value;
+    var State = document.getElementById("State").value;
+    var phone = document.getElementById("phnne").value;
+    var email = document.getElementById("email").value;
+    var Createpasswoard = document.getElementById("Createpasswoard").value;
+    var Confirmpasswoard = document.getElementById("Confirm passwoard").value;
+    var Donationamount = document.getElementById("Donation amount").value;
+    var other amount = document.getElementById("other amount").value;
+    var checkbox1 = document.getElementById("checkbox1").value;
+    var checkbox2 = document.getElementById("checkbox2").value;
+    var textarea = document.getElementById("textarea").value;
+    var button = document.getElementById("button").value;
+
+
+    alert("Successfully donate \n" +
+        "fromname:" + fromname + "\n" +
+        "name:" + Name + "\n" +
+        "lastname:" + Lastname + "\n"+
+        "Address:" + address + "\n" +
+        "city:" + city + "\n" +
+        "select:" + select + "\n" +
+        "phone:" + phone + "\n" +
+        "email:" + email + "\n" +
+        "creatpasswoard" + Createpasswoard + "\n" +
+        "confirmpasswoard:" + Confirmpasswoard + "\n" +
+        "donation:" + donationName + "\n" +
+        
+    );
